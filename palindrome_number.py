8aa63499c25bdc32b6e9eb02fb529a16a9fad1bd
Jossel Redoquerio <josselgredoquerio@iskolarngbayan.pup.edu.ph>
2024-01-22 22:41:13 +0800

    note

--- palindrome_number.py
+++ palindrome_number.py
@@ -8,12 +8,14 @@ def palindrome(number):
     print("Enter a number to check for palindrome:", number)
     original_num = number
 
+    # reverse the given number
     reverse_num = 0
     while number > 0:
         reminder = number % 10
         reverse_num = (reverse_num * 10) + reminder
         number = number // 10
-    
+
+    # check numbers
     if original_num == reverse_num:
         print("This number palindrome")
     else:
