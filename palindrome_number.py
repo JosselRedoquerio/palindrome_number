976adfeb53b0f1cde0bb28eccd36486820c53d02
Jossel Redoquerio <josselgredoquerio@iskolarngbayan.pup.edu.ph>
2024-01-22 22:34:13 +0800

    identify palindrome

--- palindrome_number.py
+++ palindrome_number.py
@@ -3,3 +3,19 @@
 
 # A palindrome number is a number that is the same after reverse. 
 # For example, 545, is the palindrome numbers
+
+def palindrome(number):
+    print("Enter a number to check for palindrome:", number)
+    original_num = number
+
+    reverse_num = 0
+    while number > 0:
+        reminder = number % 10
+        reverse_num = (reverse_num * 10) + reminder
+        number = number // 10
+    
+    if original_num == reverse_num:
+        print("This number palindrome")
+    else:
+        print("This number is not palindrome")
+
